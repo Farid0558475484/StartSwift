d817be80b49574e169cf7f4c7c43039f10bf444e
Farid0558475484 <faridmh@code.edu.az>
2023-01-31 11:40:31 +0400

    start lesson 1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var  a = 1
+
+var b = 2.1
+
+
+let c = Double(a) + b
+
+print(c)
+
+var areYouHappy = true
+
+if (areYouHappy){
+    print("happy")
+} else{
+    print("no happy")
+}
+
+ 
