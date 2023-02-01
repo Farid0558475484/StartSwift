e0ea47e67b794f307857674870a988d318af90b7
Farid0558475484 <faridmh@code.edu.az>
2023-02-01 23:28:16 +0400

    if else switch

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -19,4 +19,33 @@ if (areYouHappy){
     print("no happy")
 }
 
- 
+let x = 12
+let y = 5
+
+
+x % y
+
+var f = 1
+
+f += 1
+print(f)
+
+
+let name = "salam"
+
+if name == "salam" {
+    print("salam dunya")
+}else{
+    print("sagol")
+}
+
+name != "farid" ? print("a") : print("b")
+
+
+let age11 = 11
+
+switch age11{
+case 1...10: print("0-10")
+case 12...20: print("12-20")
+default:print("not fount")
+}
