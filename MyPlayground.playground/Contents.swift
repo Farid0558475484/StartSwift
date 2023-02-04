b5805ed459956c6d3f029adf6b7cc52fa8ee0359
Farid0558475484 <faridmh@code.edu.az>
2023-02-04 19:09:55 +0400

    function

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -6,7 +6,6 @@ var  a = 1
 
 var b = 2.1
 
-
 let c = Double(a) + b
 
 print(c)
@@ -21,7 +20,6 @@ if (areYouHappy){
 
 let x = 12
 let y = 5
-
 
 x % y
 
@@ -83,7 +81,6 @@ let fruits = [apple,orange,ananas,lemon]
 
 fruits[0]
 
-
 var set = Set<String>()
 
 var listOfSport : Set = ["Futbool", "Tennis", "Running"]
@@ -105,7 +102,6 @@ for i in listOfSport.sorted(){
 let number: Set = [5,3,5,7,3,7,27,]
 number.sorted()
 
-
 let oddNumbers : Set = [1,3,5,7,9]
 let evenNumbers : Set = [2,4,6,8]
 let primeNumbers : Set = [2,3,5,7]
@@ -126,7 +122,6 @@ var height = ["farid":1.65,"elmar":1.70]
 
 height["farid"]
 
-
 var resault = ["Mathematics":100, "Geography":99, "Russian":74]
 
 resault.count
@@ -137,7 +132,6 @@ resault
 
 resault["History"] = 55
 resault
-
 
 resault.updateValue( 50, forKey: "Mathematics")
 resault
@@ -152,3 +146,85 @@ for (key,i) in resault{
 
 var keys = Array(resault.keys)
 var values = Array(resault.values)
+
+
+//functon
+
+func square(num:Int){
+    print(num * num)
+}
+square(num:5)
+
+
+func square1(number:Int)->Int{
+    return number*number
+}
+
+var res = square1(number: 8)
+
+
+func sum1()->Int{
+    5+5
+}
+sum1()
+
+func getUset()->[String]{
+    ["Farid","Elmar"]
+}
+
+var getUset1 = getUset()
+print(getUset1[0])
+
+func getUser11()->(first:String, second:String){
+    (first:"Nicat", second:"Axundov")
+}
+let getUset12 = getUser11()
+print(getUset12.first)
+
+
+func sayHello(to name:String){
+    print("Hello \(name)")
+}
+sayHello(to: "Farid")
+
+
+func greet(_ person:String){
+    print("Hello \(person)")
+}
+greet("Salam")
+
+
+func greet1(_ person:String){
+    print("Hello \(person)")
+}
+greet1("111")
+
+
+print("Hello Word,", terminator: " ")
+print(" Hello Word,", terminator: " ")
+print(" Hello Word", terminator: " ")
+
+
+func greet123(_ person:String, _ nicely:Bool=true){
+    if nicely==true{
+        print("Hello \(person), Yes")
+    }else{
+        print("Oh no no \(person) again ... ")
+    }
+}
+
+greet123("Ferid",  false)
+
+
+func squares(num:Int...){
+    for i in num{
+        print("\(i)  squared is \(i * i)")
+    }
+}
+
+squares(num: 1,2,3,4,5)
+
+
+var ff:Int=10
+var gg:Bool=true
+print(ff + gg)
